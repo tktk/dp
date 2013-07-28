60fef9a3078410e1d076c3b23c6746dd5c5c3791
tekuto <g.tekuto@gmail.com>
2013-07-28 20:12:55 +0900

    以下の型を追加 ・dp::String ・dp::Utf8 ・dp::Utf16 ・dp::Utf32 dp::StringCharの定義を修正

--- inc/dp/common/primitives.h
+++ inc/dp/common/primitives.h
@@ -1,6 +1,8 @@
 ﻿#ifndef DP_COMMON_PRIMITIVES_H
 #define DP_COMMON_PRIMITIVES_H
 
+#include <string>
+
 namespace dp {
     typedef bool Bool;
 
@@ -13,10 +15,15 @@ namespace dp {
     typedef signed long long Long;
     typedef unsigned long long ULong;
 
-    typedef Byte StringChar;
+    typedef char StringChar;
     typedef UByte Utf8Char;
     typedef UShort Utf16Char;
     typedef UInt Utf32Char;
+
+    typedef std::basic_string< StringChar > String;
+    typedef std::basic_string< Utf8Char > Utf8;
+    typedef std::basic_string< Utf16Char > Utf16;
+    typedef std::basic_string< Utf32Char > Utf32;
 }
 
 #endif  // DP_COMMON_PRIMITIVES_H
