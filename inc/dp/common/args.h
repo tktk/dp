2a9cef610e2311924c52c67228c52b4fa1e27dab
tekuto <g.tekuto@gmail.com>
2013-07-29 14:53:44 +0900

    以下の型を追加 ・dp::Args

---
+++ inc/dp/common/args.h
@@ -0,0 +1,12 @@
+﻿#ifndef DP_COMMON_ARGS_H
+#define DP_COMMON_ARGS_H
+
+#include "dp/common/primitives.h"
+
+#include <vector>
+
+namespace dp {
+    typedef std::vector< Utf32 > Args;
+}
+
+#endif  // DP_COMMON_ARGS_H
