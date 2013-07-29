09a2ea5c76486f372b5cc4fd29416569b9591015
tekuto <g.tekuto@gmail.com>
2013-07-29 14:55:29 +0900

    linux用、windows用の以下の関数の宣言を追加 ・dp::initArgs()

---
+++ inc/dp/linux/common/args.h
@@ -0,0 +1,14 @@
+﻿#ifndef DP_LINUX_COMMON_ARGS_H
+#define DP_LINUX_COMMON_ARGS_H
+
+#include "dp/common/args.h"
+#include "dp/util/import.h"
+
+namespace dp {
+    DPEXPORT void initArgs(
+        Args &
+        , char **
+    );
+}
+
+#endif  // DP_LINUX_COMMON_ARGS_H
---
+++ inc/dp/windows/common/args.h
@@ -0,0 +1,14 @@
+﻿#ifndef DP_WINDOWS_COMMON_ARGS_H
+#define DP_WINDOWS_COMMON_ARGS_H
+
+#include "dp/common/args.h"
+#include "dp/util/import.h"
+
+namespace dp {
+    DPEXPORT void initArgs(
+        Args &
+        , wchar_t **
+    );
+}
+
+#endif  // DP_WINDOWS_COMMON_ARGS_H
