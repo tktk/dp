dbf8c4b6fe099b7d77d20eec7faab8006d09c9f9
tekuto <g.tekuto@gmail.com>
2013-09-16 20:27:01 +0900

    以下の型を追加 ・dp::GLhalf ・dp::GLuint64EXT ・dp::GLhalfARB ・dp::GLhandleARB ・dp::GLcharARB ・dp::GLsizeiptrARB ・dp::GLintptrARB ・dp::GLfixed ・dp::GLint64EXT ・dp::GLhalfNV ・dp::GLvdpauSurfaceNV

--- inc/dp/opengl/types.h
+++ inc/dp/opengl/types.h
@@ -4,36 +4,68 @@
 #include "dp/common/primitives.h"
 
 #include <cstddef>
+#include <cinttypes>
 
 namespace dp {
-    typedef UInt GLenum;
-    typedef UByte GLboolean;
+    // GL_VERSION_1_0
+    typedef unsigned int GLenum;
+    typedef float GLfloat;
+    typedef int GLint;
+    typedef int GLsizei;
     typedef void GLvoid;
+    typedef unsigned int GLbitfield;
+    typedef double GLdouble;
+    typedef unsigned int GLuint;
+    typedef unsigned char GLboolean;
+    typedef unsigned char GLubyte;
 
-    typedef Byte GLbyte;
-    typedef Short GLshort;
-    typedef Int GLint;
+    // GL_VERSION_1_1
+    typedef float GLclampf;
+    typedef double GLclampd;
 
-    typedef UByte GLubyte;
-    typedef UShort GLushort;
-    typedef UInt GLuint;
-
-    typedef Float GLfloat;
-    typedef Float GLclampf;
-    typedef Double GLdouble;
-    typedef Double GLclampd;
-
-    typedef StringChar GLchar;
-    typedef UInt GLbitfield;
-    typedef Int GLsizei;
-
-    typedef ptrdiff_t GLintptr;
+    // GL_VERSION_1_5
     typedef ptrdiff_t GLsizeiptr;
+    typedef ptrdiff_t GLintptr;
 
-    typedef Long GLint64;
-    typedef ULong GLuint64;
+    // GL_VERSION_2_0
+    typedef char GLchar;
+    typedef short GLshort;
+    typedef signed char GLbyte;
+    typedef unsigned short GLushort;
 
-    typedef struct __GLsync * GLsync;
+    // GL_VERSION_3_0
+    typedef unsigned short GLhalf;
+
+    // GL_VERSION_3_2
+    typedef struct __GLsync *GLsync;
+    typedef uint64_t GLuint64;
+    typedef int64_t GLint64;
+
+    // GL_ARB_bindless_texture
+    typedef uint64_t GLuint64EXT;
+
+    // GL_ARB_half_float_pixel
+    typedef unsigned short GLhalfARB;
+
+    // GL_ARB_shader_objects
+    typedef unsigned int GLhandleARB;   //FIXME __APPLE__の場合void *
+    typedef char GLcharARB;
+
+    // GL_ARB_vertex_buffer_object
+    typedef ptrdiff_t GLsizeiptrARB;
+    typedef ptrdiff_t GLintptrARB;
+
+    // GL_OES_fixed_point
+    typedef GLint GLfixed;
+
+    // GL_NV_gpu_shader5
+    typedef int64_t GLint64EXT;
+
+    // GL_NV_half_float
+    typedef unsigned short GLhalfNV;
+
+    // GL_NV_vdpau_interop
+    typedef GLintptr GLvdpauSurfaceNV;
 }
 
 #endif  // DP_OPENGL_TYPES_H
