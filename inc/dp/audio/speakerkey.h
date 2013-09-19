f7e0d23c5fcf4dd48aa5fc9bf1b9649073900832
tekuto <g.tekuto@gmail.com>
2013-09-19 16:25:11 +0900

    以下の構造体の宣言を追加 ・dp::SpeakerManager ・dp::SpeakerManagerInfo ・dp::SpeakerKey 以下の型を追加 ・dp::SpeakerManagerConnectEventHandler ・dp::SpeakerManagerUnique ・dp::SpeakerManagerShared ・dp::SpeakerManagerWeak ・dp::SpeakerManagerInfoUnique ・dp::SpeakerManagerInfoShared ・dp::SpeakerManagerInfoWeak ・dp::SpeakerKeyUnique ・dp::SpeakerKeyShared ・dp::SpeakerKeyWeak 以下の関数の宣言を追加 ・dp::newSpeakerManager() ・dp::free( dp::SpeakerManager & ) ・dp::getInfo() ・dp::getInfoMutable() ・dp::getMutexForConnectEventHandler() ・dp::newSpeakerManagerInfo() ・dp::clone( const dp::SpeakerManagerInfo & ) ・dp::free( dp::SpeakerManagerInfo & ) ・dp::getConnectEventHandler() ・dp::setConnectEventHandler() ・dp::clone( const dp::SpeakerKey & ) ・dp::free( dp::SpeakerKey & ) ・dp::equals( const dp::SpeakerKey &, const dp::SpeakerKey & ) ・dp::less( const dp::SpeakerKey &, const dp::SpeakerKey & )

---
+++ inc/dp/audio/speakerkey.h
@@ -0,0 +1,38 @@
+﻿#ifndef DP_AUDIO_SPEAKERKEY_H
+#define DP_AUDIO_SPEAKERKEY_H
+
+#include "dp/common/memory.h"
+#include "dp/common/primitives.h"
+#include "dp/util/import.h"
+
+#include <memory>
+
+namespace dp {
+    struct SpeakerKey;
+
+    DPEXPORT SpeakerKey * clone(
+        const SpeakerKey &
+    );
+
+    DPEXPORT void free(
+        SpeakerKey &
+    );
+
+    DPEXPORT Bool equals(
+        const SpeakerKey &
+        , const SpeakerKey &
+    );
+
+    DPEXPORT Bool less(
+        const SpeakerKey &
+        , const SpeakerKey &
+    );
+
+    typedef Unique< SpeakerKey >::type SpeakerKeyUnique;
+
+    typedef std::shared_ptr< SpeakerKey > SpeakerKeyShared;
+
+    typedef std::weak_ptr< SpeakerKey > SpeakerKeyWeak;
+}
+
+#endif  // DP_AUDIO_SPEAKERKEY_H
---
+++ inc/dp/audio/speakermanager.h
@@ -0,0 +1,79 @@
+﻿#ifndef DP_AUDIO_SPEAKERMANAGER_H
+#define DP_AUDIO_SPEAKERMANAGER_H
+
+#include "dp/common/memory.h"
+#include "dp/common/primitives.h"
+#include "dp/audio/speakerkey.h"
+#include "dp/util/import.h"
+
+#include <mutex>
+#include <functional>
+#include <memory>
+
+namespace dp {
+    struct SpeakerManager;
+
+    struct SpeakerManagerInfo;
+
+    typedef std::function<
+        void(
+            SpeakerManager &
+            , SpeakerKeyUnique &&
+            , Bool
+        )
+    > SpeakerManagerConnectEventHandler;
+
+    DPEXPORT SpeakerManager * newSpeakerManager(
+        const SpeakerManagerInfo &
+    );
+
+    DPEXPORT void free(
+        SpeakerManager &
+    );
+
+    DPEXPORT const SpeakerManagerInfo & getInfo(
+        const SpeakerManager &
+    );
+
+    DPEXPORT SpeakerManagerInfo & getInfoMutable(
+        SpeakerManager &
+    );
+
+    DPEXPORT std::mutex & getMutexForConnectEventHandler(
+        SpeakerManager &
+    );
+
+    DPEXPORT SpeakerManagerInfo * newSpeakerManagerInfo(
+    );
+
+    DPEXPORT SpeakerManagerInfo * clone(
+        const SpeakerManagerInfo &
+    );
+
+    DPEXPORT void free(
+        SpeakerManagerInfo &
+    );
+
+    DPEXPORT const SpeakerManagerConnectEventHandler & getConnectEventHandler(
+        const SpeakerManagerInfo &
+    );
+
+    DPEXPORT void setConnectEventHandler(
+        SpeakerManagerInfo &
+        , const SpeakerManagerConnectEventHandler &
+    );
+
+    typedef Unique< SpeakerManager >::type SpeakerManagerUnique;
+
+    typedef std::shared_ptr< SpeakerManager > SpeakerManagerShared;
+
+    typedef std::weak_ptr< SpeakerManager > SpeakerManagerWeak;
+
+    typedef Unique< SpeakerManagerInfo >::type SpeakerManagerInfoUnique;
+
+    typedef std::shared_ptr< SpeakerManagerInfo > SpeakerManagerInfoShared;
+
+    typedef std::weak_ptr< SpeakerManagerInfo > SpeakerManagerInfoWeak;
+}
+
+#endif  // DP_AUDIO_SPEAKERMANAGER_H
