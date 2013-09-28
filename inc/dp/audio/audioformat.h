b664fdb44ba38ea080f200f3a3b5ade28d2a6607
tekuto <g.tekuto@gmail.com>
2013-09-28 18:12:24 +0900

    以下の構造体の宣言を追加 ・dp::AudioPlayer ・dp::AudioPlayerInfo 以下の型を追加 ・dp::AudioPlayerPlayEventHandler ・dp::AudioPlayerEndEventHandler ・dp::AudioPlayerUnique ・dp::AudioPlayerShared ・dp::AudioPlayerWeak ・dp::AudioPlayerInfoUnique ・dp::AudioPlayerInfoShared ・dp::AudioPlayerInfoWeak ・dp::AudioFormat 以下の関数の宣言を追加 ・dp::newAudioPlayer() ・dp::free( const dp::AudioPlayer & ) ・dp::getInfo( const dp::AudioPlayer & ) ・dp::getInfoMutable( dp::AudioPlayer & ) ・dp::getMutexForPlayEventHandler() ・dp::getMutexForEndEventHandler() ・dp::pause() ・dp::newAudioPlayerInfo() ・dp::clone( const dp::AudioPlayerInfo & ) ・dp::free( dp::AudioPlayerInfo & ) ・dp::getPlayEventHandler() ・dp::setPlayEventHandler() ・dp::getEndEventHandler() ・dp::setEndEventHandler()

---
+++ inc/dp/audio/audioformat.h
@@ -0,0 +1,15 @@
+﻿#ifndef DP_AUDIO_AUDIOFORMAT_H
+#define DP_AUDIO_AUDIOFORMAT_H
+
+namespace dp {
+    enum AudioFormat
+    {
+        INVALID,
+
+        U8,
+        S16LE,
+        //TODO
+    };
+}
+
+#endif  // DP_AUDIO_AUDIOFORMAT_H
---
+++ inc/dp/audio/audioplayer.h
@@ -0,0 +1,108 @@
+﻿#ifndef DP_AUDIO_AUDIOPLAYER_H
+#define DP_AUDIO_AUDIOPLAYER_H
+
+#include "dp/audio/speakerkey.h"
+#include "dp/audio/audioformat.h"
+#include "dp/common/memory.h"
+#include "dp/common/primitives.h"
+#include "dp/util/import.h"
+
+#include <mutex>
+#include <functional>
+#include <memory>
+
+namespace dp {
+    struct AudioPlayer;
+
+    struct AudioPlayerInfo;
+
+    typedef std::function<
+        ULong(
+            AudioPlayer &
+            , void *
+            , ULong
+        )
+    > AudioPlayerPlayEventHandler;
+
+    typedef std::function<
+        void(
+            AudioPlayer &
+        )
+    > AudioPlayerEndEventHandler;
+
+    DPEXPORT AudioPlayer * newAudioPlayer(
+        const SpeakerKey &
+        , const AudioPlayerInfo &
+        , AudioFormat
+        , UInt
+        , UInt
+    );
+
+    DPEXPORT void free(
+        AudioPlayer &
+    );
+
+    DPEXPORT const AudioPlayerInfo & getInfo(
+        const AudioPlayer &
+    );
+
+    DPEXPORT AudioPlayerInfo & getInfoMutable(
+        AudioPlayer &
+    );
+
+    DPEXPORT std::mutex & getMutexForPlayEventHandler(
+        AudioPlayer &
+    );
+
+    DPEXPORT std::mutex & getMutexForEndEventHandler(
+        AudioPlayer &
+    );
+
+    DPEXPORT void pause(
+        AudioPlayer &
+        , Bool
+    );
+
+    DPEXPORT AudioPlayerInfo * newAudioPlayerInfo(
+    );
+
+    DPEXPORT AudioPlayerInfo * clone(
+        const AudioPlayerInfo &
+    );
+
+    DPEXPORT void free(
+        AudioPlayerInfo &
+    );
+
+    DPEXPORT const AudioPlayerPlayEventHandler & getPlayEventHandler(
+        const AudioPlayerInfo &
+    );
+
+    DPEXPORT void setPlayEventHandler(
+        AudioPlayerInfo &
+        , const AudioPlayerPlayEventHandler &
+    );
+
+    DPEXPORT const AudioPlayerEndEventHandler & getEndEventHandler(
+        const AudioPlayerInfo &
+    );
+
+    DPEXPORT void setEndEventHandler(
+        AudioPlayerInfo &
+        , const AudioPlayerEndEventHandler &
+    );
+
+    typedef Unique< AudioPlayer >::type AudioPlayerUnique;
+
+    typedef std::shared_ptr< AudioPlayer > AudioPlayerShared;
+
+    typedef std::weak_ptr< AudioPlayer > AudioPlayerWeak;
+
+    typedef Unique< AudioPlayerInfo >::type AudioPlayerInfoUnique;
+
+    typedef std::shared_ptr< AudioPlayerInfo > AudioPlayerInfoShared;
+
+    typedef std::weak_ptr< AudioPlayerInfo > AudioPlayerInfoWeak;
+}
+
+#endif  // DP_AUDIO_AUDIOPLAYER_H
