3b7a55e8243be3068782752f6f8cb8a531f5b15c
tekuto <g.tekuto@gmail.com>
2013-10-17 22:37:57 +0900

    以下の構造体の宣言を追加 ・dp::FileW 以下の型を追加 ・dp::FileWUnique ・dp::FileWShared ・dp::FileWWeak 以下の関数の宣言を追加 ・dp::newFileW() ・dp::free( const dp::FileW & ) ・dp::write()

---
+++ inc/dp/file/filew.h
@@ -0,0 +1,36 @@
+﻿#ifndef DP_FILE_FILEW_H
+#define DP_FILE_FILEW_H
+
+#include "dp/common/memory.h"
+#include "dp/common/primitives.h"
+#include "dp/util/import.h"
+
+#include <memory>
+
+namespace dp {
+    struct FileW;
+
+    DPEXPORT FileW * newFileW(
+        const Utf32 &
+    );
+
+    DPEXPORT void free(
+        FileW &
+    );
+
+    DPEXPORT Bool write(
+        FileW &
+        , const void *
+        , ULong
+    );
+
+    //TODO
+
+    typedef Unique< FileW >::type FileWUnique;
+
+    typedef std::shared_ptr< FileW > FileWShared;
+
+    typedef std::weak_ptr< FileW > FileWWeak;
+}
+
+#endif  // DP_FILE_FILEW_H
