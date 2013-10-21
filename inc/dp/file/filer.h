0fa53ac5f70287df2e69ec3f8a14a4c1165e5a40
tekuto <g.tekuto@gmail.com>
2013-10-21 20:49:20 +0900

    以下の構造体の宣言を追加 ・dp::FileR 以下の型を追加 ・dp::FileRUnique ・dp::FileRShared ・dp::FileRWeak 以下の関数の宣言を追加 ・dp::newFileR() ・dp::free( dp::FileR & ) ・dp::read()

---
+++ inc/dp/file/filer.h
@@ -0,0 +1,36 @@
+﻿#ifndef DP_FILE_FILER_H
+#define DP_FILE_FILER_H
+
+#include "dp/common/memory.h"
+#include "dp/common/primitives.h"
+#include "dp/util/import.h"
+
+#include <memory>
+
+namespace dp {
+    struct FileR;
+
+    DPEXPORT FileR * newFileR(
+        const Utf32 &
+    );
+
+    DPEXPORT void free(
+        FileR &
+    );
+
+    DPEXPORT ULong read(
+        FileR &
+        , void *
+        , ULong
+    );
+
+    //TODO
+
+    typedef Unique< FileR >::type FileRUnique;
+
+    typedef std::shared_ptr< FileR > FileRShared;
+
+    typedef std::weak_ptr< FileR > FileRWeak;
+}
+
+#endif  // DP_FILE_FILER_H
