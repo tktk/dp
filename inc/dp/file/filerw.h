feeed0912c007345bef99e69b3ad6b47c297d45f
tekuto <g.tekuto@gmail.com>
2013-10-22 20:45:24 +0900

    以下の構造体の宣言を追加 ・dp::FileRW 以下の型を追加 ・dp::FileRWUnique ・dp::FileRWShared ・dp::FileRWWeak 以下の関数の宣言を追加 ・dp::newFileRW() ・dp::free( dp::FileRW & ) ・dp::read()(データサイズ指定なし) ・dp::read()(データサイズ指定あり) ・dp::write()(データサイズ指定なし) ・dp::write()(データサイズ指定あり)

---
+++ inc/dp/file/filerw.h
@@ -0,0 +1,56 @@
+﻿#ifndef DP_FILE_FILERW_H
+#define DP_FILE_FILERW_H
+
+#include "dp/common/memory.h"
+#include "dp/common/primitives.h"
+#include "dp/util/import.h"
+
+#include <memory>
+
+namespace dp {
+    struct FileRW;
+
+    DPEXPORT FileRW * newFileRW(
+        const Utf32 &
+    );
+
+    DPEXPORT void free(
+        FileRW &
+    );
+
+    DPEXPORT Bool read(
+        FileRW &
+        , void *
+        , ULong
+        , ULong &
+    );
+
+    DPEXPORT Bool read(
+        FileRW &
+        , void *
+        , ULong &
+    );
+
+    DPEXPORT Bool write(
+        FileRW &
+        , const void *
+        , ULong
+        , ULong &
+    );
+
+    DPEXPORT Bool write(
+        FileRW &
+        , const void *
+        , ULong &
+    );
+
+    //TODO
+
+    typedef Unique< FileRW >::type FileRWUnique;
+
+    typedef std::shared_ptr< FileRW > FileRWShared;
+
+    typedef std::weak_ptr< FileRW > FileRWWeak;
+}
+
+#endif  // DP_FILE_FILERW_H
